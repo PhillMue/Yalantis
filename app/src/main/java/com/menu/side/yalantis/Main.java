a7fb4dcfd427d5553fb513772df2ecbeae64bdf3
PhillMue <muenzuve@gmail.com>
2016-03-11 14:52:04 +0300

    added fragment pakage

--- app/src/main/java/com/menu/side/yalantis/Main.java
+++ app/src/main/java/com/menu/side/yalantis/Main.java
@@ -1,7 +1,15 @@
 package com.menu.side.yalantis;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+
+import yalantis.com.sidemenu.interfaces.Resourceble;
+import yalantis.com.sidemenu.util.ViewAnimator;
 
 public class Main extends AppCompatActivity {
 
@@ -9,5 +17,23 @@ public class Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        ViewAnimator viewAnimator = new ViewAnimator<>(ActionBarActivity.this,
+                new ArrayList<Resourceble>(),
+                (LinearLayout) findViewById(R.id.left_drawer),
+                contentFragment, drawerLayout);
+        //to open menu you have to override ActionBarDrawerToggle method
     }
-}
+
+        @Override
+        public void onDrawerSlide(View drawerView, float slideOffset) {
+            super.onDrawerSlide(drawerView, slideOffset);
+            if (slideOffset > 0.6 && viewAnimator.getLinearLayout().getChildCount() == 0)
+                viewAnimator.showMenuContent();
+        }
+        public void onDrawerClosed(View view) {
+            super.onDrawerClosed(view);
+            viewAnimator.getLinearLayout().removeAllViews();
+            viewAnimator.getLinearLayout().invalidate();
+        }
+    }
+
---
+++ app/src/main/java/com/menu/side/yalantis/fragment/ContentFragment.java
@@ -0,0 +1,7 @@
+package com.menu.side.yalantis.fragment;
+
+/**
+ * Created by mue on 3/11/16.
+ */
+public class ContentFragment {
+}
